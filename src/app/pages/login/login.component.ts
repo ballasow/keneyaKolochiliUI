f637ea182cefaa7c0964fe06da0a675e0fcb27a9
ballasow <122132979+ballasow@users.noreply.github.com>
2026-07-13 15:32:35 +0000

    update

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card'; // <--- AJOUTEZ CETTE LIGNE
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule // <--- AJOUTEZ CE MODULE ICI
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  // Renommez onSubmit en login pour correspondre à votre HTML
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Connexion réussie', this.loginForm.value);
+    }
+  }
+}
