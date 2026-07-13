a1946b2d4129e7fc78f14776d2a85df85bd5d73b
Chitan Founé Mallé <mchitanfoune@gmail.com>
2026-07-13 10:02:35 +0000

    ajout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProfileAdmin } from './pages/admin/profile-admin/profile-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile-admin',
+    component: ProfileAdmin
+  }
+];
---
+++ src/app/pages/admin/profile-citoyen/profile-citoyen.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-citoyen',
+  imports: [],
+  templateUrl: './profile-citoyen.html',
+  styleUrl: './profile-citoyen.scss',
+})
+export class ProfileCitoyen {}
